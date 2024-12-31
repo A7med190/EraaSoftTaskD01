41e3d7ebd9c990150e1a276517f1d7a172f96d26
hp <hp@DESKTOP-15N9FA4>
2024-12-31 15:03:59 +0200

    Add project files.

---
+++ TaskD01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace TaskD01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n                Islam's Carpet Cleaning Service    \n");
+            Console.WriteLine("\n Estimate for carpet cleaning service\n");
+            Console.WriteLine("Number of small carpets:");
+            int sc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number of large carpets:");
+            int lc = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Price per small room: $25");
+            Console.WriteLine("Price per large room: $35\n");
+            Console.WriteLine($"Cost : ${(sc*25)+(lc*35)}");
+            Console.WriteLine("Tax  : $6.6");
+            Console.WriteLine("===============================\n");
+            Console.WriteLine($"Total estimate: ${(sc * 25) + (lc * 35) + 6.6}");
+            Console.WriteLine("This estimate is valid for 30 days\n");
+
+
+
+
+        }
+    }
+}
